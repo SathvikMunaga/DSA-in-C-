d728c585708e3ed144e6a94d94350f1e67435440
mnsathvik123@gmail.com <mnsathvik123@gmail.com>
2025-12-11 11:26:32 +0530

    DAY 2

--- All Patterns/Basic_paterns.cpp
+++ All Patterns/Basic_paterns.cpp
@@ -50,5 +50,87 @@ int main()
         }
         cout<<endl;
     }
+    int r = 4;
+    for(int i = 0; i<r; i++) {
+        for(int j = 0; j< 4-(i+1); j++){
+            cout<<" ";
+        }
+        for(int j = 0; j< i+1; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    int t = 4; 
+    for(int i = 0; i < t; i++){
+        for(int j = 0; j<i; j++){
+            cout<<" ";
+        }
+        for(int j = 1; j<n-i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    //pyramid
+    int y = 4;
+    for(int i = 0; i < y; i++){
+        for(int j = 0; j < n-1-i; j++){
+            cout<<" ";
+        }
+        for(int j = 0; j < 2*i+1; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    //inverted pyramid
+    int u = 4;
+    for (int i = 0; i<u; i++){
+        for(int j = 0; j<i; j++){
+            cout<<" ";
+        }
+        for(int j = 1; j < n - i; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    //diamond pattern
+    int o = 4;
+    for(int i = 0; i < o; i++){
+        for(int j = 0; j < o - 1 - i; j++){
+            cout<<" ";
+        }
+        for(int j = 0; j < i+1; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    for(int i = 0; i < o-1; i++){
+        for (int j = 0; j < i+1; j++){
+            cout<<" ";
+        }
+        for(int j = 0; j< o - i -1; j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    //again hallow diamond
+    int p = 4;
+    for(int i =0; i < p; i++){
+        for(int j = 0; j < p-i-1; j++){
+            cout<<" ";
+        }
+        cout<<"*";
+        if(i!=0){
+            for(int j = 0; j< 2*i-1; j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+        cout<<endl;
+    }
     return 0;
 }
