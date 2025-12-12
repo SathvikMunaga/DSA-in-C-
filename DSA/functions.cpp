85e2b49c9dbda55298db54d6b92650380e62cc23
mnsathvik123@gmail.com <mnsathvik123@gmail.com>
2025-12-12 13:25:03 +0530

    END of the day

--- DSA/functions.cpp
+++ DSA/functions.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 // sum of n numbers
 int SumOfn(int n)
@@ -48,14 +49,18 @@ int ncr(int n, int r)
     int fact_nmr = factorial(n - r);
     return fact_n / (fact_r * fact_nmr);
 }
-//finding whether a number is prime or not
-int prime(int n){
-    if(n <= 1){
+// finding whether a number is prime or not
+int prime(int n)
+{
+    if (n <= 1)
+    {
         // cout<< "The given number is not a PRIME NUMBER"<<endl;
         return 0;
     }
-    for(int i = 2; i*i<=n; i++){
-        if( n % i == 0){
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+        {
             // cout<<"The given number is not a PRIME NUMBER"<<endl;
             return 0;
         }
@@ -63,13 +68,64 @@ int prime(int n){
     // cout<<"It's a PRIME NUMBER"<<endl;
     return 1;
 }
-//printing all the prime numbers from 2 to n
-void primeupton(int n){
-    for(int i = 2; i<=n; i++){
-        if(prime(i)){
-            cout<<i<<" "<<endl;
+// printing all the prime numbers from 2 to n
+void primeupton(int n)
+{
+    for (int i = 2; i <= n; i++)
+    {
+        if (prime(i))
+        {
+            cout << i << " " << endl;
         }
     }
+}
+// reversing number
+int reverse(int n)
+{
+    int rev = 0;
+    while (n > 0)
+    {
+        int lastdig = n % 10;
+        rev = rev * 10 + lastdig;
+        n = n / 10;
+    }
+    return rev;
+}
+// trying palindrome number
+void palindrome(int n)
+{
+    if(n == reverse(n)){
+        cout<<"It is a palindrom";
+    }else{
+        cout<<"It is not a palindrome";
+    }
+    return;
+}
+int isArmstrong(int n) {
+    int original = n;
+    int digits = 0;
+    
+    // Count digits
+    int temp = n;
+    while (temp > 0) {
+        digits++;
+        temp /= 10;
+    }
+
+    // Calculate sum of digits^digits
+    int sum = 0;
+    temp = n;  // reset temp
+    while (temp > 0) {
+        int lastDigit = temp % 10;
+        sum += pow(lastDigit, digits);
+        temp /= 10;
+    }
+
+    // Compare
+    if (sum == original)
+        cout<< "YES";  // Armstrong
+    else
+        cout<< "NO";  // Not Armstrong
 }
 int main()
 {
@@ -79,6 +135,9 @@ int main()
     // cout << "sum = " << sumofdig(2356)<<endl;
     // cout << ncr(8, 2)<<endl ;
     // prime(10);
-    primeupton(10);
+    // primeupton(10);
+    // cout << reverse(89) << endl;
+    // palindrome(1241);
+    isArmstrong(656);
     return 0;
 }
