17a922eb7fe50e504a4ad09006bdbcf9aa014e01
mnsathvik123@gmail.com <mnsathvik123@gmail.com>
2025-12-16 14:33:30 +0530

    BIT WISE

--- binary_nums.cpp
+++ binary_nums.cpp
@@ -15,5 +15,16 @@ int main(){
     cout<< ans<<endl;
 
     //binary to decimal numnbers
+    int binum = 1111;
+    int pow2 = 1;
+    int rem2;
+    int ans2 = 0;
+    while(binum > 0){
+        rem2 = binum % 2;
+        binum = binum / 10;
+        ans2 = ans2 + (rem2 * pow2);
+        pow2 *= 2;
+    }
+    cout<< ans2<<endl;
     return 0;
 }
