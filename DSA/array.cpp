a2dc6f22db60babcb4a0fd7b32bffc0175d653a2
mnsathvik123@gmail.com <mnsathvik123@gmail.com>
2025-12-18 17:08:11 +0530

    END of ARRAY lecture

--- DSA/array.cpp
+++ DSA/array.cpp
@@ -12,6 +12,17 @@ int linear_search(int arr[], int size, int target)
     }
     return -1;
 }
+//function for reversing an array
+void reverse(int arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+    while(start < end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
 int main(){
     // int marks[5] = {91,82,93,64,55};
     // int size = 5;
@@ -63,9 +74,16 @@ int main(){
     //     cout<<endl;
 
         //array for linear search 
-        int array[] = {1,2,3,4,5,6,7};
-        int size2 = 6;
-        int target = 4;
-        cout<<linear_search(array,size2,target);
+        // int array[] = {1,2,3,4,5,6,7};
+        // int size2 = 6;
+        // int target = 4;
+        // cout<<linear_search(array,size2,target);
+        int array1[] = {1,2,3,4,5,6,7,8,9};
+        int size = sizeof(array1) / sizeof(int);
+        reverse(array1, size);
+        for(int i = 0; i < size; i++)
+        {
+            cout<<array1[i]<<" ";
+        }
         return 0;
 }
