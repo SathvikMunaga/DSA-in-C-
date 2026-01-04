c86dae972ea4c9a474ef73265cf04175e7b32002
mnsathvik123@gmail.com <mnsathvik123@gmail.com>
2026-01-04 09:44:44 +0530

    Binary

--- binary_nums.cpp
+++ binary_nums.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main(){
     //converting decimal umbers into binary numbers using code
-    int decnum = 18;
+    int decnum = 21;
+    int rem;
     int pow = 1;
     int ans = 0;
-    int rem;
-    while(decnum>0){
+    while(decnum > 0){
         rem = decnum % 2;
         decnum = decnum / 2;
-        ans = ans + (rem*pow);
-        pow = pow*10;
+        ans = ans + (rem * pow);
+        pow *= 10;
     }
     cout<< ans<<endl;
 
