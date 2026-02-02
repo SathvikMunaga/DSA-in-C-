bb869ee05b614951d3485925a136db54a5f4d346
mnsathvik123@gmail.com <mnsathvik123@gmail.com>
2026-02-02 18:40:48 +0530

    Bubble sort

---
+++ DSA/bubble_sort .cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void bubble_sort(vector<int>& nums)
+{
+    int n = nums.size();
+    for(int i = n - 1; i > 0; i--)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            if(nums[j] > nums[j + 1])
+            {
+                int temp = nums[j+1];
+                nums[j+1] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+}
+int main()
+{
+    vector<int> nums = {1,4,6,3,9,7,544,64,8};
+    bubble_sort(nums);
+    for(int el : nums){
+        cout<<el<<endl;
+    }
+    return 0;
+}
